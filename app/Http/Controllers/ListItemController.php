fdd87c82a04106c4fbad8dcd17ef85135e5ec3ae
Bramantia Dewantara <dewantarabram86@gmail.com>
2025-04-30 00:39:23 +0700

    membuat kerangka MVC dari P3

---
+++ app/Http/Controllers/ListItemController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListItemController extends Controller
+{
+    public function index()
+    {
+        return view('list_item');
+    }
+
+}
